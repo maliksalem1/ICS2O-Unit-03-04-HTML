fb36c1bf80ab5d3550cbb6827cd1f2d59ec5dd39
maliksalem1 <99354681+maliksalem1@users.noreply.github.com>
2022-04-11 14:17:55 +0000

    readme

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Created by: maliksalem1
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates volume and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const length = parseInt(document.getElementById('Tlength').value)
+  const width = parseInt(document.getElementById('width').value)
+  const height = parseInt(document.getElementById('height').value)
+
+  // process 
+  const volume = (length * width * height) / 3
+
+  // output
+  document.getElementById('volume').innerHTML = '<p>volume is: ' + volume.toString() + ' cm³</p>'
+}
